de7e46cfa9bfdf670851b192f25acbe00ae8cc74
Roger Rajaratnam <roger@sourcier.uk>
2025-07-17 00:56:39 +0100

    Recommended VSCode Settings with automatic import sorting with ESLint

--- README.md
+++ README.md
@@ -67,3 +67,11 @@ export default tseslint.config([
   },
 ])
 ```
+
+## Recommended VSCode Settings
+
+To align your VSCode settings with the project's recommended configuration:
+
+1. Navigate to the `.vscode` directory at the root of the project.
+2. Copy the `settings.example.json` file and rename the copy to `settings.json`.
+3. If desired, customize the `settings.json` with your personal preferences.
--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import { globalIgnores } from "eslint/config";
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -20,4 +21,13 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+]);
