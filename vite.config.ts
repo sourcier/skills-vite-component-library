5055c93a3341f53a2d9ea4a21ea9779efd5e7e67
Roger Rajaratnam <roger@sourcier.uk>
2025-07-17 01:25:39 +0100

    Basic build setup, enable Vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+  },
 });
