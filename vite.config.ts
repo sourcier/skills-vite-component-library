7d3adf6f6e3ed43ef6e0825b0ccff0e0385e52fe
Roger Rajaratnam <roger@sourcier.uk>
2025-07-17 01:36:48 +0100

    Generate type files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ tsconfigPath: "./tsconfig.lib.json" })],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
