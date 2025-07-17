8f61ac8d31a19b763b4b05668bf7fe8c1039cd7a
Roger Rajaratnam <roger@sourcier.uk>
2025-07-17 01:56:18 +0100

    Add CSS imports to generated JS

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({ tsconfigPath: "./tsconfig.lib.json", rollupTypes: true }),
   ],
   build: {
